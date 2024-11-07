f7edbe263960d9b2b7c96f93ff0c769e4b965d04
kimit0310 <kimit0310@gmail.com>
2024-11-07 12:31:25 -0500

    forgot to remove in-line comment, now has attributes, removing Y-MARGIN for now

--- src/MoBI_GUI/config.py
+++ src/MoBI_GUI/config.py
@@ -5,8 +5,12 @@ This module contains the Config class and a function to set up loggers(soon).
 
 
 class Config:
-    """Configuration class holding application-wide settings."""
+    """Configuration class holding application-wide settings.
+
+    Attributes:
+        BUFFER_SIZE: Size of the buffer for storing data samples.
+        TIMER_INTERVAL: Timer interval in milliseconds for data acquisition.
+    """
 
     BUFFER_SIZE: int = 1000
-    Y_MARGIN: float = 0.1
-    TIMER_INTERVAL: int = 50  # in milliseconds
+    TIMER_INTERVAL: int = 50
